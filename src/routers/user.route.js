4d54889366d112c7579bf5b7ab6266bcf62c968e
rojon <rjrojon@gmail.com>
2021-07-04 16:38:49 +0600

    user Router setting up loging, profile, signup

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -1,28 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../db/models/User.model');
+const auth = require('../middleware/auth')
 
-router.post('/', async (req,res)=>{
+//Login a user
+router.post('/login', async (req,res)=>{
     const user = new User(req.body);
     try {
         await user.save();
-        res.status(200).send("User is created.")
+        const token = await user.generateAuthToken();
+        res.status(200).send({user,token})
     } catch (error) {
         res.send(error)
     }
 })
 
-router.get('/:id',async (req,res) =>{
-    const id = req.params.id;
+
+//Create a user
+router.post('/', async (req, res) => {
+    const user = new User(req.body);
     try {
-        const user = await User.findById(id);
-        if(!user) {
-            res.send("User not found");
-        }
-        res.send(user)
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.status(201).send({user,token});
+    } catch (error) {
+        res.send(error);
+    }
+})
+
+//Get user Profile
+router.get('/me', auth, (req, res)=>{
+    try {
+        res.send(req.user)       
     } catch (error) {
         res.send(error)
     }
 })
+
+
 
 module.exports = router;
