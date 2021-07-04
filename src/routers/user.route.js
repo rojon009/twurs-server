de2eacb62cefc1087f19f696b8824f4fd3958f7b
rojon <rjrojon@gmail.com>
2021-07-04 17:00:19 +0600

    user router login logout added login has problem yet

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -37,6 +37,48 @@ router.get('/me', auth, (req, res)=>{
     }
 })
 
+//User Login
+router.post('/login', async (req,res)=>{
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        console.log(user)
+        if(!user) {
+            res.send("NOT FOUND");
+        }
+
+        const token = await user.generateAuthToken();
+
+        res.status(200).send({user,token});
+    } catch (error) {
+        res.send(error);
+    }
+})
+
+
+//User Logout
+router.post('/logout',auth, async (req,res)=>{
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token)
+        const user = await req.user.save();
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+
+
+//Get All users for testing
+router.get("/all", async ( req, res ) => {
+    try {
+        const users = await User.find({});
+        if(!users) return res.send('NO USER IS CREATED');
+        res.send(users)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
 
 
 module.exports = router;
