0c7017a03be976c65b6faf126bd797bf216c19bd
rojon <rjrojon@gmail.com>
2021-07-04 16:18:19 +0600

    change in user model for jwt auth

--- src/db/models/User.model.js
+++ src/db/models/User.model.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -10,16 +14,85 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
+        unique: true,
         lowercase: true,
-        unique: true
+        validate(email) {
+            if(!validator.isEmail(email)) {
+                throw new Error('Email must be a valid email');
+            }
+        }
     },
-    orders: [
-        {
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        // validate(password) {
+        //     if(!validator.isStrongPassword(password)) {
+        //         throw new Error('Password must be Strong')
+        //     }
+        // }
+    },
+    orders: [{
+        product: {
             type: mongoose.Types.ObjectId,
             ref: 'Product'
         }
-    ]
+    }],
+    tokens: [{
+        token:{
+            type: String,
+            required: true
+        }
+    }]
+}, {
+    timestamps: true
+});
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+
+    const token = jwt.sign({_id: user._id.toString()}, 'twurshiringtask');
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+    return token;
+}
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject;
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email});
+    if(!user) {
+        throw new Error('Email or password not matched');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+        throw new Error('Email or password not matched');
+    }
+
+    return user;
+}
+
+// Hash the plain password before save
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
 })
+
+
 
 const User = mongoose.model('User',userSchema)
 
