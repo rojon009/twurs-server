96739457ee6d42728efe8664a6c5109e087965f4
rojon <rjrojon@gmail.com>
2021-07-05 18:57:16 +0600

    isAdmin middleware added for authenticate admin

---
+++ src/middleware/isAdmin.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const Admin = require('../db/models/Admin.model');
+
+const isAdmin = async (req,res, next) => {
+    try {
+        const token = await req.header('Authorization').replace('Bearer ', '');
+        const decodedToken = await jwt.verify(token,'twurshiringtask');
+        const admin = await Admin.findOne({_id: decodedToken._id, 'tokens.token': token});
+        if(!admin) {
+            throw new Error();
+        }
+        req.admin = admin;
+        req.token = token;
+        next();
+        
+    } catch (error) {
+        res.status(401).send({err: 'You Are not Admin'})
+    }
+}
+
+module.exports = isAdmin;
